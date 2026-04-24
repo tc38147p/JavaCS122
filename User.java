5ab28fee91fc08f3e1f6ae513b7dd883d996aa77
BP92452P <brianpc444@gmail.com>
2026-04-24 13:58:34 -0400

    User class/Event management
    hey

---
+++ User.java
@@ -0,0 +1,16 @@
+public class User {
+  String username;
+  String password;
+  Event[] events = new Event[50]
+  int eventCount = 0;
+
+  public User(String u, String p) {
+    username = u;
+    password = p;
+  }
+
+  public void addEvent(Event e) {
+    events[eventCount] = e;
+    eventCount++;
+  }
+}
