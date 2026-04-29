f8ae1f47e3cb02dfc17821ec8f743360a18bb943
BP92452P <brianpc444@gmail.com>
2026-04-29 12:48:57 -0400

    weather predict base
    base

--- WeatherPredictor.java
+++ WeatherPredictor.java
@@ -1,1 +1,24 @@
 public class WeatherPredictor {
+
+  String[] weeklyWeather = {
+    "   ", "   "
+
+  };
+
+
+
+  int[] daysBeforeMonth = {
+    0,   //January
+    31,  //February
+    59,  //March
+
+
+  };
+
+  int dayOfYear = daysBeforeMonth[month - 1] + day;
+  return (dayOfYear - 1) / 7;
+
+public String predict(int month, int day) {
+  int week = getWeekNumber(month, day);
+  return weeklyWeather[week];
+}
