25f33c953885fc67dd5ea1c8222171ee793d37a0
BP92452P <brianpc444@gmail.com>
2026-04-29 12:53:11 -0400

    holiday base

--- HolidayManager.java
+++ HolidayManager.java
@@ -1,1 +1,22 @@
 public class HolidayManager { 
+
+  String[] holidayNames = {
+    "New Years Day",
+    "Valentine's Day",
+
+
+
+  };
+
+  int[] holidayMonths = {1, 2};
+  int[] holidayDays = {1, 14};
+
+  public String getHoliday(int month, int day) {
+    for (int i = 0; i < holidayNames.length; i++) {
+      if (holidayMonths[i] == month && holidayDays[i] == day) {
+        return holidayNames[i];
+      }
+    }
+    return null;
+  }
+}
