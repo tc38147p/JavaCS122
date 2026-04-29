d290248d1d15e508146f98443d8bbff986a38b22
tc38147p <tc38147p@pace.edu>
2026-04-29 14:12:45 -0400

    Holidays UD

--- HolidayManager.java
+++ HolidayManager.java
@@ -2,14 +2,26 @@ public class HolidayManager {
 
   String[] holidayNames = {
     "New Years Day",
+    "Martin Luther King Day",
     "Valentine's Day",
-
-
-
+    "Presidents Day",
+    "Saint Patricks Day",
+    "Easter",
+    "Mother's Day",
+    "Memorial Day",
+    "Juneteenth",
+    "Independence Day",
+    "International Beer Day",
+    "Labor Day",
+    "Columbus Day",
+    "Halloween",
+    "Veterans Day",
+    "Thanksgiving day",
+    "Christmas Day"
   };
 
-  int[] holidayMonths = {1, 2};
-  int[] holidayDays = {1, 14};
+  int[] holidayMonths = {1,1,2,2,3,3,4,5,5,6,7,8,9,10,10,11,11,12}; 
+  int[] holidayDays = {1,19,14,16,17,28,1,9,31,19,4,6,6,11,31,11,25,25};
 
   public String getHoliday(int month, int day) {
     for (int i = 0; i < holidayNames.length; i++) {
