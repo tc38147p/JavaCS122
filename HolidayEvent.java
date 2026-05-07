ff15e084c58ccc935b30835955ccddb3c081bbb6
BP92452P <brianpc444@gmail.com>
2026-05-07 12:10:31 -0400

    This is it

--- HolidayEvent.java
+++ HolidayEvent.java
@@ -1,1 +1,12 @@
 public class HolidayEvent extends Event {
+  String holidayName;
+
+  public HolidayEvent(int m, ind d, String a, String h) {
+    super(m, d, a);
+    holidayName = h;
+  }
+
+  public String getDescription() {
+    return super.fetDescription() + "Holiday: " + holidayName + ")";
+  }
+}
