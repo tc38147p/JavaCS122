2197e45fe95cd00856f3d44cc8f8408b71e3ed5c
BP92452P <brianpc444@gmail.com>
2026-05-07 12:12:46 -0400

    syntax

--- HolidayEvent.java
+++ HolidayEvent.java
@@ -1,12 +1,12 @@
 public class HolidayEvent extends Event {
   String holidayName;
 
-  public HolidayEvent(int m, ind d, String a, String h) {
+  public HolidayEvent(int m, int d, String a, String h) {
     super(m, d, a);
     holidayName = h;
   }
 
   public String getDescription() {
-    return super.fetDescription() + "Holiday: " + holidayName + ")";
+    return super.getDescription() + "Holiday: " + holidayName + ")";
   }
 }
